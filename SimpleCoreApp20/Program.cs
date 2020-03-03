e2d1627a393c6f8cffad717426dfd89045a6aca1
Feodor Fitsner <feodor@appveyor.com>
2020-03-02 19:41:35 -0800

    Add yml

---
+++ SimpleCoreApp20/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SimpleCoreApp20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
